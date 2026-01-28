25fd0f06f729e7b23f8e02ae7e326a89e1bf4c86
Ebrahim Ahmed <www.hyma26@gmail.com>
2026-01-28 22:04:24 +0200

    make all components responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,109 @@
+// import logo from "../../assets/Logo.png";
+// import { NavLink, Link } from "react-router-dom";
+// import style from "./navbar.module.css";
+
+// const Navbar = () => {
+//   return (
+//     <nav className={style.nav}>
+//       <Link to="/">
+//         <img src={logo} alt="SignBridge Logo" style={{ height: "45px" }} />
+//       </Link>
+
+//       <ul className={style.navLinks}>
+//         <li>
+//           <NavLink to="/">Home</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/sign-to-text">Sign to Text</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/text-to-sign">Text to Sign</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/learning">Learning</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/about">About us</NavLink>
+//         </li>
+//       </ul>
+
+//       <div className={style.auth}>
+//         <NavLink to="/login" className={style.loginBtn}>
+//           Login
+//         </NavLink>
+//         <NavLink to="/signup" className={style.signupBtn}>
+//           SignUp
+//         </NavLink>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+import React, { useState } from "react"; // إضافة useState
 import logo from "../../assets/Logo.png";
-import { NavLink, Link } from "react-router-dom"; // استعملنا NavLink للينكات
+import { NavLink, Link } from "react-router-dom";
 import style from "./navbar.module.css";
+import { FaBars, FaTimes } from "react-icons/fa"; // استيراد الأيقونات
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false); // حالة القائمة
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <nav className={style.nav}>
-      <Link to="/">
+      <Link to="/" className={style.logoContainer}>
         <img src={logo} alt="SignBridge Logo" style={{ height: "45px" }} />
       </Link>
 
-      <ul className={style.navLinks}>
+      {/* أيقونة الموبايل */}
+      <div className={style.menuIcon} onClick={toggleMenu}>
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      {/* الروابط - ستتغير كلاساتها بناءً على الحالة */}
+      <ul className={`${style.navLinks} ${isOpen ? style.activeMenu : ""}`}>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/" onClick={() => setIsOpen(false)}>
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/sign-to-text">Sign to Text</NavLink>
+          <NavLink to="/sign-to-text" onClick={() => setIsOpen(false)}>
+            Sign to Text
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/text-to-sign">Text to Sign</NavLink>
+          <NavLink to="/text-to-sign" onClick={() => setIsOpen(false)}>
+            Text to Sign
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/learning">Learning</NavLink>
+          <NavLink to="/learning" onClick={() => setIsOpen(false)}>
+            Learning
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/about">About us</NavLink>
+          <NavLink to="/about" onClick={() => setIsOpen(false)}>
+            About us
+          </NavLink>
         </li>
+
+        {/* أزرار تسجيل الدخول داخل القائمة في الموبايل */}
+        <div className={style.mobileAuth}>
+          <NavLink to="/login" onClick={() => setIsOpen(false)}>
+            Login
+          </NavLink>
+          <NavLink
+            to="/signup"
+            className={style.signupBtn}
+            onClick={() => setIsOpen(false)}
+          >
+            SignUp
+          </NavLink>
+        </div>
       </ul>
 
       <div className={style.auth}>
